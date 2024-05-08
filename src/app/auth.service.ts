59a968daf8d56b3d48f81392fd236a764a1c685b
Krushnakant Shinde <shindekrushna825@gmail.com>
2024-05-08 15:33:40 +0530

    add auth token logic

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Subject } from 'rxjs';
-import { catchError, switchMap } from 'rxjs/operators';
-import { of, throwError } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { catchError, switchMap } from "rxjs/operators";
+import { of, throwError } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
   public $refreshToken = new Subject<boolean>();
@@ -18,22 +18,22 @@ export class AuthService {
 
   // Get stored access token
   getAccessToken(): string | null {
-    return localStorage.getItem('jwtToken');
+    return localStorage.getItem("jwtToken");
   }
 
   // Get stored refresh token
   getRefreshToken(): string | null {
-    return localStorage.getItem('refreshToken');
+    return localStorage.getItem("refreshToken");
   }
 
   // Store new access token
   setAccessToken(token: string): void {
-    localStorage.setItem('accessToken', token);
+    localStorage.setItem("accessToken", token);
   }
 
   // Store new refresh token
   setRefreshToken(token: string): void {
-    localStorage.setItem('refreshToken', token);
+    localStorage.setItem("refreshToken", token);
   }
 
   // Refresh the access token
@@ -41,20 +41,22 @@ export class AuthService {
     const refreshToken = this.getRefreshToken();
 
     if (!refreshToken) {
-      return throwError('No refresh token available.');
+      return throwError("No refresh token available.");
     }
 
     const body = { refreshToken };
 
-    return this.http.post('http://10.0.51.3:8091/users/refreshToken', body).pipe(
-      switchMap((response: any) => {
-        this.setAccessToken(response.accessToken); // Store the new access token
-        return of(response.accessToken);
-      }),
-      catchError((error) => {
-        console.error('Failed to refresh token:', error);
-        return throwError(error);
-      })
-    );
+    return this.http
+      .post("https://10.0.51.3:8091/users/refreshToken", body)
+      .pipe(
+        switchMap((response: any) => {
+          this.setAccessToken(response.accessToken); // Store the new access token
+          return of(response.accessToken);
+        }),
+        catchError((error) => {
+          console.error("Failed to refresh token:", error);
+          return throwError(error);
+        })
+      );
   }
 }
