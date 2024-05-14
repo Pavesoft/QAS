bd3c9ab82ec01e0aaea4d6ab1bd1f270645144f3
Krushnakant Shinde <shindekrushna825@gmail.com>
2024-05-14 14:21:32 +0530

    add refresh token logic logout.

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Subject } from "rxjs";
+import { Inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject, throwError } from "rxjs";
 import { catchError, switchMap } from "rxjs/operators";
-import { of, throwError } from "rxjs";
+import { TopbarComponent } from "./header/topbar/topbar.component";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
+  topbarComponent = Inject(TopbarComponent);
+
   public $refreshToken = new Subject<boolean>();
 
   constructor(private http: HttpClient) {
     this.$refreshToken.subscribe(() => {
-      this.refreshAccessToken().subscribe(); // Automatically refresh the token when the subject is triggered
+      this.refreshAccessToken().subscribe();
     });
   }
 
@@ -48,14 +50,21 @@ export class AuthService {
 
     return this.http.post("https://technonxt.in/users/refreshToken", body).pipe(
       switchMap((response: any) => {
-        // console.log("refresh token ", response.jwtToken);
+        console.log("called function", response.jwtToken);
         localStorage.setItem("jwtToken", response.jwtToken); // Store the new access token
-        return of(response.jwtToken);
+        return response.jwtToken; // Return the new token
       }),
-      catchError((error) => {
-        console.error("Failed to refresh token:", error);
+      catchError((error: any) => {
+        if (error.status === 401) {
+          localStorage.removeItem("jwtToken");
+          localStorage.removeItem("refreshToken"); // Remove the JWT token
+
+          window.location.reload(); // Trigger logout event
+        }
         return throwError(error);
       })
     );
   }
+
+  // Observable to notify about logout
 }
