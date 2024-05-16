e633a670596c8099183cbc844aa93fa75dc1b7ec
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-16 19:38:57 +0530

    change baseURl to localhost

--- const.ts
+++ const.ts
@@ -1,2 +1,2 @@
-// export const baseURl = "https://technonxt.in";
-export const baseURl = "https://technonxt.in";
+export const baseURl = "https://localhost:8888";
+// export const baseURl = "http://10.0.51.3:8090";
