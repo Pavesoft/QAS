c5c72afac59b42b4594a856aa22f38c38a8198c3
Krushnakant Shinde <shindekrushna825@gmail.com>
2024-05-17 14:57:27 +0530

    add my-profile page with form.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.scss"],
 })
 export class ProfileComponent {
+  content: string = "My Profile";
+  passwordForm: FormGroup;
+  isEditing = false;
 
+  constructor(private fb: FormBuilder) {
+    this.passwordForm = this.fb.group(
+      {
+        password: ["", [Validators.required]],
+        confirmPassword: ["", [Validators.required]],
+      },
+      { validator: this.passwordsMatch }
+    );
+  }
+  passwordsMatch(control: AbstractControl): { [key: string]: boolean } | null {
+    const password = control.get("password");
+    const confirmPassword = control.get("confirmPassword");
+    if (password?.value !== confirmPassword?.value) {
+      return { mismatch: true };
+    }
+    return null;
+  }
+  displayContent(content: string) {
+    this.content = content;
+  }
+  toggleEdit() {
+    this.isEditing = !this.isEditing;
+  }
 }
