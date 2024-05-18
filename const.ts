dd4ea767fc0bb65b5b68ee821b9b09b2d70d59a3
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-05-18 12:29:49 +0530

    url change to backend.QS

--- const.ts
+++ const.ts
@@ -1,2 +1,3 @@
 // export const baseURl = "http://10.0.51.3:8090";
-export const baseURl = "https://technonxt.in";
+// export const baseURl = "https://technonxt.in";
+export const baseURl = "https://backend.quadrant-solutions.com";
