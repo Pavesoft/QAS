497494f80ed1c0fd6f5360ec24ab6ba2fadba08f
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-06-01 15:57:17 +0530

    www url redirection check

--- src/app/Services/urlMappingService.ts
+++ src/app/Services/urlMappingService.ts
@@ -18,6 +18,30 @@ export class UrlMappingService {
   constructor(private httpClient: HttpClient) {}
 
   getMapping(fromUrl: string): Observable<string | undefined> {
+    function addWwwToUrl(url: any) {
+      // Use a regular expression to identify the protocol and domain
+      const urlPattern = /^(https?:\/\/)([^/]+)/;
+
+      // Replace the domain with "www." if it doesn't already start with "www."
+      const modifiedUrl = url.replace(
+        urlPattern,
+        (match: any, protocol: any, domain: any) => {
+          if (!domain.startsWith("www.")) {
+            return `${protocol}www.${domain}`;
+          }
+          return match; // Return the original match if "www." is already present
+        }
+      );
+
+      return modifiedUrl;
+    }
+
+    // const url =
+    //   "https://quadrant-solutions.com/market_research/2018-emerging-star-iot-security-market-device-identity-management/";
+    const newUrl = addWwwToUrl(fromUrl);
+
+    // console.log(newUrl);
+
     return this.httpClient
       .get<UrlMapping[]>("../../assets/fonts/Redirection_url.json")
       .pipe(
@@ -25,7 +49,7 @@ export class UrlMappingService {
           this.urlMappings = data;
           const mapping = _.find(
             this.urlMappings,
-            (m) => m.fromUrl === fromUrl + "/"
+            (m) => m.fromUrl === fromUrl + "/" || m.fromUrl === newUrl
           );
           return mapping ? mapping.toUrl : undefined;
         })
