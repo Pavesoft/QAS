424604aebe7ac08b052f553eb144f078a4481c64
Akash Agarwal <akash.agarwal@pavesoft.co>
2024-08-02 14:00:20 +0530

    updated backend url

--- const.ts
+++ const.ts
@@ -1,3 +1,3 @@
 // export const baseURl = "https://technonxt.in";
-export const baseURl = "http://10.0.51.3:8091";
-// export const baseURl = "https://backend.quadrant-solutions.com";
+// export const baseURl = "http://10.0.51.3:8091";
+export const baseURl = "https://backend.quadrant-solutions.com";
